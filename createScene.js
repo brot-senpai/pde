e68b41433a8a54c6eff0c96909c4c886632f161f
brot-senpai <74334704+brot-senpai@users.noreply.github.com>
2020-12-14 07:24:00 -0500

    Add files via upload

---
+++ createScene.js
@@ -0,0 +1,13 @@
+var createScene = function(props) {
+	var {engine, canvas} = props;
+    var scene = new BABYLON.Scene(engine);
+    const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene);
+    camera.attachControl(canvas, true);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+
+    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {segments: 64, diameter: 2});
+
+  return scene;
+};
+
+export { createScene };
